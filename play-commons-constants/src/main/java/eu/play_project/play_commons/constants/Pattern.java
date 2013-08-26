47ec291080c3007c1945f91e897ac9f33dd8ec79
stuehmer <roland.stuehmer@fzi.de>
2013-08-26 15:28:00 +0200

    Add constants for pattern REST service

---
+++ play-commons-constants/src/main/java/eu/play_project/play_commons/constants/Pattern.java
@@ -0,0 +1,10 @@
+package eu.play_project.play_commons.constants;
+
+public class Pattern {
+
+	/**
+	 * The path section for event patterns used when creating pattern URIs and
+	 * when exposing the corresponding Web Service.
+	 */
+	public static final String PATTERN_PATH = "/patterns/";
+}
